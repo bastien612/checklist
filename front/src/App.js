a86163802a26e6a6dcd0d9d74debf21cbd6c82f9
bameunie <bastien.meunier@yahoo.fr>
2018-10-16 18:44:17 +0200

    add margin top

--- front/src/App.js
+++ front/src/App.js
@@ -10,7 +10,7 @@ export default class App extends Component {
     return (
       <React.Fragment>
           <Header />
-          <div>Pouet</div>
+          <div style={{ marginTop:"100px" }}>Pouet</div>
           <Footer />
       </React.Fragment>
     );
