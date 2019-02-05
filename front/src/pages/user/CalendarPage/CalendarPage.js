e23ace9884a75367fbb936a755576a0ce0b52995
bastien612 <bastien.meunier@yahoo.fr>
2019-02-05 08:49:21 +0100

    Add Big Calendar and react big scheduler to test

--- front/src/pages/user/CalendarPage/CalendarPage.js
+++ front/src/pages/user/CalendarPage/CalendarPage.js
@@ -1,33 +1,88 @@
-import { withStyles } from '@material-ui/core';
+import { withStyles, Grid } from '@material-ui/core';
 import moment from 'moment';
 import React from 'react';
-import BigCalendar from 'react-big-calendar';
+import Scheduler, { SchedulerData, ViewTypes, DATE_FORMAT } from 'react-big-scheduler'
+import 'react-big-scheduler/lib/css/style.css'
 
 const styles = () => ({
 
 })
 
-const localizer = BigCalendar.momentLocalizer(moment) // or globalizeLocalizer
+const groups = [{ id: 1, title: 'Thibaud' }, { id: 2, title: 'Lucas' }]
+const resources = [
+    {
+        id: 'r1',
+        name: 'Resource1',
+    },
+    {
+        id: 'r2',
+        name: 'Resource2',
+    }]
+const events = [
+    {
+        id: 1,
+        start: '2017-12-18 09:30:00',
+        end: '2017-12-19 23:30:00',
+        resourceId: 'r1',
+        title: 'I am finished',
+        bgColor: '#D9D9D9'
+    },
+    {
+        id: 2,
+        start: '2017-12-18 12:30:00',
+        end: '2017-12-26 23:30:00',
+        resourceId: 'r2',
+        title: 'I am not resizable',
+        resizable: false
+    },
+    {
+        id: 3,
+        start: '2017-12-19 12:30:00',
+        end: '2017-12-20 23:30:00',
+        resourceId: 'r3',
+        title: 'I am not movable',
+        movable: false
+    },
+    {
+        id: 4,
+        start: '2017-12-19 14:30:00',
+        end: '2017-12-20 23:30:00',
+        resourceId: 'r1',
+        title: 'I am not start-resizable',
+        startResizable: false
+    },
+    {
+        id: 5,
+        start: '2017-12-19 15:30:00',
+        end: '2017-12-20 23:30:00',
+        resourceId: 'r2',
+        title: 'R2 has recurring tasks every week on Tuesday, Friday',
+        rrule: 'FREQ=WEEKLY;DTSTART=20171219T013000Z;BYDAY=TU,FR',
+        bgColor: '#f759ab'
+    }
+];
 
-const myEventsList = [{
-    title: "pouet",
-    start: new Date('December 17, 2018 03:24:00'),
-    end: new Date('December 18, 2018 10:24:00'),
-    allDay: true
-}]
+let schedulerData = new SchedulerData(new moment().format(DATE_FORMAT), ViewTypes.day);
+schedulerData.setLocaleMoment(moment);
+schedulerData.setResources(resources);
+schedulerData.setEvents(events);
+
+// const defaultTimeStart = moment().hours(7);
+// const defaultTimeEnd = moment().hours(22);
 
 class CategoryPage extends React.Component {
 
     render() {
         return (
-            <div >
-                <BigCalendar
-                    localizer={localizer}
-                    events={myEventsList}
-                    startAccessor="start"
-                    endAccessor="end"
-                />
-            </div >
+            <Grid container justify="center" alignItems="center">
+
+                <Grid item xs={10}>
+                    <Scheduler
+                        schedulerData={schedulerData}
+
+                    />
+                </Grid>
+            </Grid>
         )
     }
 }
