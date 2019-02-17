37af546bffa197375da6ff2a0b65d79841b13904
bastien612 <bastien.meunier@yahoo.fr>
2019-02-17 15:52:13 +0100

    Improve Map to put a marker where user ckick

--- front/src/pages/user/MapPage/MapPage.js
+++ front/src/pages/user/MapPage/MapPage.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { CardHeader, Typography, Grid, withStyles } from '@material-ui/core';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import Header from "components/Layouts/Header";
+
 
 const styles = theme => ({
 
@@ -21,27 +23,88 @@ class MapPage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            lat: 51.505,
-            lng: -0.09,
-            zoom: 13,
+            initLat: 44.825810,
+            initLng: -0.620837,
+            initZoom: 13,
+            markersCoord: shops,
+            currentId: 3
         };
+
+        this.mapRef = React.createRef();
+    }
+
+
+    handleClick = (e) => {
+        console.log("click latnlng : " + JSON.stringify(e.latlng));
+        this.addMarker(e.latlng);
+
+        // if (map != null) {
+        //     map.leafletElement.locate();
+        // }
+    }
+
+    addMarker = (latlng) => {
+        const map = this.mapRef.current
+        if (map != null) {
+            const coordsCopy = JSON.parse(JSON.stringify(this.state.markersCoord));
+            coordsCopy.push({ id: this.state.currentId, coord: [latlng.lat, latlng.lng] })
+            console.log("coordsCopy :" + JSON.stringify(coordsCopy));
+            this.setState({
+                markersCoord: coordsCopy,
+                currentId: this.state.currentId + 1
+            })
+        }
+    }
+
+    handleLocationFound = (e) => {
+        this.addMarker(e.latlng);
     }
 
     render() {
-        const position = [this.state.lat, this.state.lng]
-        return (
-            <div>
-                Coucou
+        console.log(this.state.markersCoord);
 
-                <Map center={position} zoom={this.state.zoom} style={{ height: '350px' }}>
-                    <TileLayer
-                        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-                </Map>
-            </div>
+        const position = [this.state.initLat, this.state.initLng];
+
+        const markers = this.state.markersCoord.length !== 0 ? this.state.markersCoord.map(obj => {
+            return (
+                <Marker key={obj.id} position={obj.coord}>
+                    <Popup>You are here</Popup>
+                </Marker>
+            )
+        }) : null;
+
+        return (
+            <Grid container spacing={16} justify="center">
+                <Grid item xs={12} >
+                    <Header />
+                </Grid>
+                <Grid item xs={6} >
+                    <Map center={position}
+                        zoom={this.state.initZoom}
+                        style={{ height: '400px' }}
+                        onClick={this.handleClick}
+                        onLocationfound={this.handleLocationFound}
+                        ref={this.mapRef}
+                    >
+                        <TileLayer
+                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                        {markers}
+
+                    </Map>
+                </Grid>
+            </Grid>
 
         )
     }
 }
+
+const shops = [{
+    id: 1,
+    coord: [44.843545, -0.649821]
+}, {
+    id: 2,
+    coord: [44.830260, -0.620298]
+}]
 
 export default withStyles(styles)(MapPage);
