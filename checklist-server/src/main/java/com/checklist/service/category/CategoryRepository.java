83910f5ccd922997e9b327d4199afabc44700e96
bastien612 <bastien.meunier@yahoo.fr>
2019-03-23 23:08:43 +0100

    huge refacto to make 2 module in the project

--- checklist-server/src/main/java/com/checklist/service/category/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.checklist.service.category;
-
-import com.checklist.model.Category;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface CategoryRepository extends PagingAndSortingRepository<Category, Long> {
-
-    public Category findByName(String name);
-}
--- checklist-server/src/main/java/com/checklist/service/product/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.checklist.service.product;
-
-import com.checklist.model.Product;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface ProductRepository extends PagingAndSortingRepository<Product, Long> {
-    Product findByName(String name);
-}
