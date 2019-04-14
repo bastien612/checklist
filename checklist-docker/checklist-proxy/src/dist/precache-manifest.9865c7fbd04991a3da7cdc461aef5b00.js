2d3e37b42def20aae2f12a19cacb5b6138d13735
Netsab612 <bastien.meunier@yahoo.fr>
2019-04-14 23:14:44 +0200

    add all dockerfiles which were ignore because of '**/build/ in .gitignore

---
+++ checklist-docker/checklist-proxy/src/dist/precache-manifest.9865c7fbd04991a3da7cdc461aef5b00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "79593bf7972281ac692e",
+    "url": "/static/js/main.79593bf7.chunk.js"
+  },
+  {
+    "revision": "3d25a606d5cc9a9cecdb",
+    "url": "/static/js/1.3d25a606.chunk.js"
+  },
+  {
+    "revision": "79593bf7972281ac692e",
+    "url": "/static/css/main.923a7c89.chunk.css"
+  },
+  {
+    "revision": "3d25a606d5cc9a9cecdb",
+    "url": "/static/css/1.486251e3.chunk.css"
+  },
+  {
+    "revision": "4ab0fc62522b43ad2e1f6ca7ff55a73d",
+    "url": "/index.html"
+  }
+];
