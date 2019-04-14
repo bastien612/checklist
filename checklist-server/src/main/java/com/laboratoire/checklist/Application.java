36e72e72897c790d2d3c919d48c6a2a69a3072e0
Netsab612 <bastien.meunier@yahoo.fr>
2019-04-14 10:23:35 +0200

    Add run of flyway migrations at starting + try fixing the migration which is not working

--- checklist-server/src/main/java/com/laboratoire/checklist/Application.java
+++ checklist-server/src/main/java/com/laboratoire/checklist/Application.java
@@ -1,5 +1,6 @@
 package com.laboratoire.checklist;
 
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
--- checklist-server/src/main/resources/db/migration/V1.1__innitial_model.sql
+++ checklist-server/src/main/resources/db/migration/V1.1__innitial_model.sql
@@ -3,26 +3,26 @@ create table category (
     name TEXT,
     created_at DATE,
     updated_at DATE
-)
+);
 
 create table product (
     id BIGINT PRIMARY KEY,
     name text,
     created_at DATE,
     updated_at DATE
-)
+);
 
 create table category_product (
     product_id BIGINT references product(id),
     category_id BIGINT references category(id)
-)
+);
 
 create table rel_product_list (
     id BIGINT PRIMARY KEY,
     title text,
     created_at DATE,
     updated_at DATE
-)
+);
 
 create table rel_product_list(
     product_id BIGINT product(id),
